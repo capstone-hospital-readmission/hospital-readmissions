74627108bd0be2850d042385f19325fe71946fb0
Qinghua <jtlqh2005@gmail.com>
2019-09-08 10:37:56 -0400

    grouping classes for race/admission/discharge/age/source_id

--- myData.py
+++ myData.py
@@ -1,35 +1,96 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
+def group_specialty(x):
+    """
+    Cardiology: 0
+    General pratice: 1
+    Internal medicine: 2
+    Surgery: 3
+    Missing: 4
+    Others: 5
+    """
+    if 'Cardiology' in x:
+        return 0
+    elif 'GeneralPractice' in x:
+        return 1
+    elif 'InternalMedicine' in x:    
+        return 2
+    elif 'Surgery' in x:
+        return 3
+    elif '?' in x:
+        return 4
+    else: 
+        return 5
+
+def group_race(x):
+    """ 
+    0: Caucasian, 
+    1: AfricanAmerican, 
+    2: missing, 
+    3: others 
+    """
+    if x in ['Caucasian']:
+        return 0
+    elif x in  ['AfricanAmerican']:
+        return 1
+    elif x in ['?']:
+        return 2
+    else:
+        return 3
+
+
+def group_admission(x):
+    """0: by refer, 
+    1: from emergency room, 
+    2: other 
+    """
+    if x in [1, 2, 3]:
+        return 0
+    elif x in [7]:
+        return 1
+    else:
+        return 2
+#
+def group_discharge(x):
+    """
+    0: others, 
+    1: discharged to home 
+    """
+    return int(x==1)
 
 def group_age(x):
-        
+        """ 
+        age 0-30: 0
+        age 30-60: 1
+        age 60-100: 2
+        """
         if x in ['[0-10)', '[10-20)', '[20-30)']:
-            return '[0-30)'
+            return 0
         elif x in ['[30-40)', '[40-50)', '[50-60)']:
-            return '[30-60)'
-        elif x in ['[60-70)', '[70-80)', '[80-90)']:
-            return '[60-90)'
+            return 1
         else:
-            return '[90-100)'
+            return 2
 
+def group_readmitted(x):
+    """
+    <30 days: 1
+    Others: 0
+    """
+    if x in ['<30']:
+        return 1     
+    else:
+        return 0
+        
 def get_data(filePath, labelEncode=False, hotEncode=False):
     
     data = pd.read_csv(filePath)
     #print(data.columns)
     print('raw data shape {}'.format(data.shape))
 
-    # group age into 4 classes
-    data['age'] = data['age'].apply(group_age)
-
+    
     misList = data.columns[data.isin(['?']).any()].tolist()
-
-    #print('Missing value percentage:')
-    #for x in misList:
-    #    print('{0:<20} {1:.2f}%'.format(x, data[x].isin(['?']).sum()/data.shape[0]*100))
-
-    # reduce readmitted class from three classes (>30, <30, No) to two classes (Yes, No) 
-    data.readmitted = data.readmitted.apply(lambda x: 'Yes' if x in ['<30'] else 'No')
+ 
 
     # remove duplicate patient_nbr
     idx = data[['encounter_id', 'patient_nbr']].groupby('patient_nbr').min().reset_index().index
@@ -38,6 +99,14 @@ def get_data(filePath, labelEncode=False, hotEncode=False):
     # remove expired discharge_disposition_id entries
     idx = data[data['discharge_disposition_id'].apply(lambda x: x in [11,19,20,21])].index
     data.drop(idx, inplace=True)
+
+    # regroup some variables
+    data['age'] = data['age'].apply(group_age)
+    data['discharge_disposition_id'] = data['discharge_disposition_id'].apply(group_discharge)
+    data['race'] = data['race'].apply(group_race)
+    data['admission_source_id'] = data['admission_source_id'].apply(group_admission)
+    data['medical_specialty'] = data['medical_specialty'].apply(group_specialty)
+    data['readmitted'] = data['readmitted'].apply(group_readmitted)
 
 
     # convert some categorical variables to numerical variables
